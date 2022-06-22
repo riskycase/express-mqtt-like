1cf2b69fbfee6decd70156f27a791ec0a2cdd2cc
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2022-06-22 12:26:31 +0530

    Server client management
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express, { static as serveStatic } from 'express';
-import { Server } from 'ws';
+import { Server, WebSocket } from 'ws';
 import { join } from 'path';
+
+interface socketStatus {
+	socket: WebSocket;
+	timer: NodeJS.Timeout;
+	isAlive: boolean;
+}
+
+let webSocketStatuses: socketStatus[] = [];
 
 const app = express();
 
@@ -8,7 +16,31 @@ app.use(serveStatic(join(__dirname, '..', 'public')));
 
 const wss = new Server({ noServer: true });
 wss.on('connection', socket => {
-	socket.on('message', message => console.log(message));
+	webSocketStatuses.push({
+		socket,
+		isAlive: true,
+		timer: setInterval(() => {
+			const socketIndex = webSocketStatuses.findIndex(
+				status => status.socket === socket
+			);
+			if (!webSocketStatuses[socketIndex].isAlive) socket.terminate();
+			else {
+				webSocketStatuses[socketIndex].isAlive = false;
+				socket.ping();
+			}
+		}, 15000),
+	});
+	socket.on('pong', () => {
+		webSocketStatuses.find(status => status.socket === socket).isAlive =
+			true;
+	});
+	socket.on('close', () => {
+		const index = webSocketStatuses.findIndex(
+			status => status.socket === socket
+		);
+		clearInterval(webSocketStatuses[index].timer);
+		webSocketStatuses.splice(index, 1);
+	});
 });
 
 const server = app.listen(process.env.PORT || 3000);
